6db77fcd88fb248c0e1576f23e186e9583ed0c11
İsa Diliballı <isadiliballi@outlook.com>
2019-07-03 21:06:10 +0300

    Initial Commit

---
+++ OnMyMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnMyMind
+//
+//  Created by İsa Diliballı on 3.07.2019.
+//  Copyright © 2019 İsa Diliballı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
