d729cb5987f85f32b3f16893b5aadcb913817f80
İsa Diliballı <isadiliballi@icloud.com>
2019-07-12 02:11:49 +0300

    Responsive ile ilgili özellikler eklendi.

--- OnMyMind/TreeLetterController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  treelettergame.swift
-//  OnMyMind
-//
-//  Created by İsa Diliballı on 4.07.2019.
-//  Copyright © 2019 İsa Diliballı. All rights reserved.
-//
-
-import Foundation
--- OnMyMind/ViewController.swift
+++ OnMyMind/ViewController.swift
@@ -17,17 +17,19 @@ class ViewController: UIViewController {
     var coins = 200
    
     @IBOutlet weak var coinstext: UILabel!
-    @IBOutlet weak var settingimage: UIImageView!
+
+    
     @IBOutlet weak var threelettertext: UILabel!
-    @IBOutlet weak var threeletterimage: UIImageView!
     @IBOutlet weak var threeletterbuttonoutlet: UIButton!
+    
+    @IBOutlet weak var fourletterbuttonoutlet: UIButton!
+    @IBOutlet weak var fourlettertext: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        threeletterimage.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        threeletterbuttonoutlet.frame = CGRect(x: threeletterimage.frame.origin.x, y: threeletterimage.frame.origin.y, width: threeletterimage.frame.size.width, height: threeletterimage.frame.size.height)
-        
+     
+      
         let dbrandom = Int.random(in: 1...5)
         let dbrandomstring = String(dbrandom)
         
@@ -39,12 +41,64 @@ class ViewController: UIViewController {
         coinstext.text = String(coins)
         
        
+        background()
+        responsive()
+    
+        
+     //   threelettertext.frame.origin.x = (self.view.bounds.size.width - threelettertext.frame.size.width) / 2
+      //  threelettertext.frame.origin.y = (threeletterbuttonoutlet.bounds.size.height - threelettertext.frame.size.height) / 2
+       // threelettertext.frame = CGRect(x: threelettertext.frame.origin.x, y: threelettertext.frame.origin.y, width: 100, height: 100)
+       // threelettertext.font = UIFont(name: "Marker Felt Wide", size: 50)
         
     }
 
+    
+    func background()
+    {
+        let backgroundImageView = UIImageView()
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backgroundImageView.image = UIImage(named: "arkaplan")
+        backgroundImageView.layer.zPosition = -1
+    }
+    
+    func responsive() {
+        let screenheight = view.frame.size.height
+        let screenwidth = view.frame.size.width
+        let ratio = screenheight + screenwidth
+        
+        if 888...1150 ~= ratio  { // iPhone Series
+            print("iPhone")
+        }
+        else if 1187...1310 ~= ratio { // iPhone X Series
+            threeletterbuttonoutlet.frame.origin.x = (self.view.bounds.size.width - threeletterbuttonoutlet.frame.size.width) / 2
+            threeletterbuttonoutlet.frame = CGRect(x: threeletterbuttonoutlet.frame.origin.x, y: threeletterbuttonoutlet.frame.origin.y, width: 284, height: 98)
+            
+            fourletterbuttonoutlet.frame.origin.x = (self.view.bounds.size.width - fourletterbuttonoutlet.frame.size.width) / 2
+            fourletterbuttonoutlet.frame = CGRect(x: fourletterbuttonoutlet.frame.origin.x, y: fourletterbuttonoutlet.frame.origin.y, width: 284, height: 98)
+            print("iPhone X Series")
+        }
+        else if ratio == 2028 { // iPad Pro 11 inch
+            print("iPad Pro 11 inç")
+        }
+        else if 1792...2390 ~= ratio { // iPad Series
+            threelettertext.frame.origin.x = (threeletterbuttonoutlet.bounds.size.width) / 2
+            threelettertext.frame = CGRect(x: threelettertext.frame.origin.x, y: threelettertext.frame.origin.y, width: 250, height: 70)
+            threelettertext.textAlignment = .center
+            threelettertext.font = UIFont(name: "Helvetica", size: 80)
+            print("iPad Series")
+        }
+    }
+    
+  
+
     @IBAction func treelettergo(_ sender: Any) {
         UIView.transition(with: threelettertext, duration: 0.2, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-        UIView.transition(with: threeletterimage, duration: 0.2, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        UIView.transition(with: threeletterbuttonoutlet, duration: 0.2, options: .transitionFlipFromLeft, animations: nil, completion: nil)
         if asd.isEmpty == true {
             print("İnternet bağlantınızı kontrol edin veya tekrar deneyin")
         }
@@ -55,6 +109,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func fourlettergo(_ sender: Any) {
+        UIView.transition(with: fourlettertext, duration: 0.2, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        UIView.transition(with: fourletterbuttonoutlet, duration: 0.2, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
