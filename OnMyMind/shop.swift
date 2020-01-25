313ec2e8868e97180a25c48357ddeca20151f976
İsa Diliballı <isadiliballi@icloud.com>
2020-01-26 02:59:48 +0300

    Responsive
    Market,
    Ücretsiz kısmı tamamlamdı.
    Altın kısmı tamamlandı.

--- OnMyMind/shop.swift
+++ OnMyMind/shop.swift
@@ -19,7 +19,6 @@ class shop: UIViewController {
     var gamegosound : AVAudioPlayer?
     var sound = true
     
-    @IBOutlet weak var darkbackground: UIImageView!
     
     @IBOutlet weak var chanceoutlet: UILabel!
     @IBOutlet weak var againturnoutlet: UILabel!
@@ -33,6 +32,18 @@ class shop: UIViewController {
     @IBOutlet weak var coinoutlet: UIButton!
     @IBOutlet weak var freeoutlet: UIButton!
     @IBOutlet weak var storeoutlet: UIButton!
+    
+    @IBOutlet weak var shoptext: UILabel!
+    
+    @IBOutlet weak var hometext: UILabel!
+    @IBOutlet weak var coinstext: UILabel!
+    @IBOutlet weak var freetext: UILabel!
+    @IBOutlet weak var storetext: UILabel!
+    
+    @IBOutlet weak var chancebuy: UIImageView!
+    @IBOutlet weak var againturnbuy: UIImageView!
+    @IBOutlet weak var hintbuy: UIImageView!
+    
     
     var dark = false
     
@@ -59,8 +70,9 @@ class shop: UIViewController {
         againturnoutlet.text = String(againturn)
         hintoutlet.text = String(hint)
 
-        darkbackground.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
         background()
+        responsive()
     }
     func background() {
         let backgroundImageView = UIImageView()
@@ -211,4 +223,131 @@ class shop: UIViewController {
     }
    
     
+    func responsive() {
+              
+              let screenheight = view.frame.size.height
+              let screenwidth = view.frame.size.width
+              let ratio = screenheight + screenwidth
+              
+              if 1042...1150 ~= ratio  { // iPhone 6 - 6 Plus - 6S - 6S Plus - 7 - 7 Plus - 8 - 8 Plus Series
+                  print("iPhone 6 - 6 Plus - 6S - 6S Plus - 7 - 7 Plus - 8 - 8 Plus Series")
+              }
+              else if ratio == 888 { // iPhone 5 - 5S - 5C - SE Series
+                  print("iPhone 5 - 5S - 5C - SE Series")
+              }
+              else if ratio == 1187 || ratio == 1310 { // iPhone X - XS - 11 Pro - XR - XS Max - 11 - 11 Pro Max Series  +
+                  print("iPhone X - XS - 11 Pro - XR - XS Max - 11 - 11 Pro Max Series")
+                
+                shoptext.font = shoptext.font.withSize(70)
+                
+                homeoutlet.frame = CGRect(x: homeoutlet.frame.origin.x, y: homeoutlet.frame.origin.y, width: homeoutlet.frame.width, height: homeoutlet.frame.width)
+                hometext.frame = CGRect(x: hometext.frame.origin.x, y: homeoutlet.frame.maxY, width: hometext.frame.width, height: hometext.frame.height)
+                
+                freeoutlet.frame = CGRect(x: freeoutlet.frame.origin.x, y: freeoutlet.frame.origin.y, width: freeoutlet.frame.width, height: freeoutlet.frame.width)
+                freetext.frame = CGRect(x: freetext.frame.origin.x, y: freeoutlet.frame.maxY, width: freetext.frame.width, height: freetext.frame.height)
+                
+                coinoutlet.frame = CGRect(x: coinoutlet.frame.origin.x, y: coinoutlet.frame.origin.y, width: coinoutlet.frame.width, height: coinoutlet.frame.width)
+                coinstext.frame = CGRect(x: coinstext.frame.origin.x, y: coinoutlet.frame.maxY, width: coinstext.frame.width, height: coinstext.frame.height)
+                
+                storeoutlet.frame = CGRect(x: storeoutlet.frame.origin.x, y: storeoutlet.frame.origin.y, width: storeoutlet.frame.width, height: storeoutlet.frame.width)
+                storetext.frame = CGRect(x: storetext.frame.origin.x, y: storeoutlet.frame.maxY, width: storetext.frame.width, height: storetext.frame.height)
+                
+                let chancebuttonhight = chancebuttonoutlet.frame.width / 9.6
+                chancebuttonoutlet.frame = CGRect(x: chancebuttonoutlet.frame.origin.x, y: chancebuttonoutlet.frame.origin.y - 10, width: chancebuttonoutlet.frame.width, height: chancebuttonhight)
+                
+                let againturnbuttonhight = againturnbuttonoutlet.frame.width / 9.6
+                againturnbuttonoutlet.frame = CGRect(x: againturnbuttonoutlet.frame.origin.x, y: againturnbuttonoutlet.frame.origin.y - 10, width: againturnbuttonoutlet.frame.width, height: againturnbuttonhight)
+                
+                let hintbuttonhight = hintbuttonoutlet.frame.width / 9.6
+                hintbuttonoutlet.frame = CGRect(x: hintbuttonoutlet.frame.origin.x, y: hintbuttonoutlet.frame.origin.y - 10, width: hintbuttonoutlet.frame.width, height: hintbuttonhight)
+                
+                chanceoutlet.font = chanceoutlet.font.withSize(20)
+                againturnoutlet.font = againturnoutlet.font.withSize(20)
+                hintoutlet.font = hintoutlet.font.withSize(20)
+              }
+                /*
+              else if ratio == 1310 { // iPhone XR - XS Max - 11 - 11 Pro Max
+               print("iPhone XR - XS Max - 11 - 11 Pro Max")
+                
+                shoptext.font = shoptext.font.withSize(70)
+                
+                homeoutlet.frame = CGRect(x: homeoutlet.frame.origin.x, y: homeoutlet.frame.origin.y, width: homeoutlet.frame.width, height: homeoutlet.frame.width)
+                hometext.frame = CGRect(x: hometext.frame.origin.x, y: homeoutlet.frame.maxY, width: hometext.frame.width, height: hometext.frame.height)
+                
+                freeoutlet.frame = CGRect(x: freeoutlet.frame.origin.x, y: freeoutlet.frame.origin.y, width: freeoutlet.frame.width, height: freeoutlet.frame.width)
+                freetext.frame = CGRect(x: freetext.frame.origin.x, y: freeoutlet.frame.maxY, width: freetext.frame.width, height: freetext.frame.height)
+                
+                coinoutlet.frame = CGRect(x: coinoutlet.frame.origin.x, y: coinoutlet.frame.origin.y, width: coinoutlet.frame.width, height: coinoutlet.frame.width)
+                coinstext.frame = CGRect(x: coinstext.frame.origin.x, y: coinoutlet.frame.maxY, width: coinstext.frame.width, height: coinstext.frame.height)
+                
+                storeoutlet.frame = CGRect(x: storeoutlet.frame.origin.x, y: storeoutlet.frame.origin.y, width: storeoutlet.frame.width, height: storeoutlet.frame.width)
+                storetext.frame = CGRect(x: storetext.frame.origin.x, y: storeoutlet.frame.maxY, width: storetext.frame.width, height: storetext.frame.height)
+                
+                let chancebuttonhight = chancebuttonoutlet.frame.width / 9.6
+                chancebuttonoutlet.frame = CGRect(x: chancebuttonoutlet.frame.origin.x, y: chancebuttonoutlet.frame.origin.y - 10, width: chancebuttonoutlet.frame.width, height: chancebuttonhight)
+                
+                let againturnbuttonhight = againturnbuttonoutlet.frame.width / 9.6
+                againturnbuttonoutlet.frame = CGRect(x: againturnbuttonoutlet.frame.origin.x, y: againturnbuttonoutlet.frame.origin.y - 10, width: againturnbuttonoutlet.frame.width, height: againturnbuttonhight)
+                
+                let hintbuttonhight = hintbuttonoutlet.frame.width / 9.6
+                hintbuttonoutlet.frame = CGRect(x: hintbuttonoutlet.frame.origin.x, y: hintbuttonoutlet.frame.origin.y - 10, width: hintbuttonoutlet.frame.width, height: hintbuttonhight)
+                
+                chanceoutlet.font = chanceoutlet.font.withSize(20)
+                againturnoutlet.font = againturnoutlet.font.withSize(20)
+                hintoutlet.font = hintoutlet.font.withSize(20)
+              } 
+              else if ratio == 2028 { // iPad Pro 11 inch
+                  print("iPad Pro 11 inch")
+              }
+              else if ratio == 2390 { // iPad Pro 12.9 inch
+                  print("iPad Pro 12.9 inch")
+              } */
+              else if 1792...2390 ~= ratio { // iPad Series
+                  print("iPad Series")
+                
+                shoptext.font = shoptext.font.withSize(view.frame.width / 7)
+                
+                let chancewidth = chancebuttonoutlet.frame.width * 0.7
+                let chancehight = chancewidth / 9.6
+                chancebuttonoutlet.frame = CGRect(x: view.frame.width / 2 - chancewidth / 2, y: chancebuttonoutlet.frame.origin.y, width: chancewidth, height: chancehight)
+                
+                let againturnwidth = againturnbuttonoutlet.frame.width * 0.7
+                let againturnhight = againturnwidth / 9.6
+                againturnbuttonoutlet.frame = CGRect(x: view.frame.width / 2 - againturnwidth / 2, y: againturnbuttonoutlet.frame.origin.y, width: againturnwidth, height: againturnhight)
+                
+                let hintwidth = hintbuttonoutlet.frame.width * 0.7
+                let hinthight = hintwidth / 9.6
+                hintbuttonoutlet.frame = CGRect(x: view.frame.width / 2 - hintwidth / 2, y: hintbuttonoutlet.frame.origin.y, width: hintwidth, height: hinthight)
+                
+                let coinwidth = coinoutlet.frame.height
+                coinoutlet.frame = CGRect(x: view.frame.width / 2 - coinwidth / 2 - 65, y: coinoutlet.frame.origin.y, width: coinwidth, height: coinwidth)
+                
+                let freewidth = freeoutlet.frame.height
+                freeoutlet.frame = CGRect(x: view.frame.width / 2 - freewidth / 2 + 65, y: freeoutlet.frame.origin.y, width: freewidth, height: freewidth)
+                
+                let homewidth = homeoutlet.frame.height
+                homeoutlet.frame = CGRect(x: view.frame.width / 2 - homewidth / 2 - 195, y: homeoutlet.frame.origin.y, width: homewidth, height: homewidth)
+                
+                let storewidth = storeoutlet.frame.height
+                storeoutlet.frame = CGRect(x: view.frame.width / 2 - storewidth / 2 + 195, y: storeoutlet.frame.origin.y, width: storewidth, height: storewidth)
+                
+                coinstext.font = coinstext.font.withSize(20)
+                freetext.font = coinstext.font.withSize(20)
+                hometext.font = coinstext.font.withSize(20)
+                storetext.font = coinstext.font.withSize(20)
+                
+                coinstext.frame = CGRect(x: coinoutlet.frame.midX - coinstext.frame.width / 2, y: coinstext.frame.origin.y, width: coinstext.frame.width, height: coinstext.frame.height)
+                
+                 hometext.frame = CGRect(x: homeoutlet.frame.midX - hometext.frame.width / 2, y: hometext.frame.origin.y, width: hometext.frame.width, height: hometext.frame.height)
+                
+                freetext.frame = CGRect(x: freeoutlet.frame.midX - freetext.frame.width / 2, y: freetext.frame.origin.y, width: freetext.frame.width, height: freetext.frame.height)
+                
+                 storetext.frame = CGRect(x: storeoutlet.frame.midX - storetext.frame.width / 2, y: storetext.frame.origin.y, width: storetext.frame.width, height: storetext.frame.height)
+                
+                chanceoutlet.font = chanceoutlet.font.withSize(30)
+                againturnoutlet.font =  againturnoutlet.font.withSize(30)
+                hintoutlet.font = hintoutlet.font.withSize(30)
+                            
+              }
+          }
 }
--- OnMyMind/shopfree.swift
+++ OnMyMind/shopfree.swift
@@ -31,6 +31,8 @@ class shopfree: UIViewController, GADRewardBasedVideoAdDelegate {
     var interstitial: GADInterstitial!
     var interstitial2: GADInterstitial!
     
+    
+    @IBOutlet weak var shoptext: UILabel!
     @IBOutlet weak var darkbackground: UIImageView!
     @IBOutlet weak var warningoutlet: UIImageView!
     @IBOutlet weak var warningtextoutlet: UILabel!
@@ -43,6 +45,14 @@ class shopfree: UIViewController, GADRewardBasedVideoAdDelegate {
     @IBOutlet weak var coinoutlet: UIButton!
     @IBOutlet weak var freeoutlet: UIButton!
     @IBOutlet weak var storeoutlet: UIButton!
+    @IBOutlet weak var goldboiler: UIImageView!
+    @IBOutlet weak var freechance: UIImageView!
+    @IBOutlet weak var freehint: UIImageView!
+    @IBOutlet weak var freeagainturn: UIImageView!
+    @IBOutlet weak var freetext: UILabel!
+    @IBOutlet weak var storetext: UILabel!
+    @IBOutlet weak var coinstext: UILabel!
+    @IBOutlet weak var hometext: UILabel!
     
     var dark = false
     
@@ -72,6 +82,7 @@ class shopfree: UIViewController, GADRewardBasedVideoAdDelegate {
         sound = UserDefaults.standard.object(forKey: "sound") as! Bool
         
         background()
+        responsive()
     }
     
     func background() {
@@ -272,4 +283,146 @@ class shopfree: UIViewController, GADRewardBasedVideoAdDelegate {
                    freeoutlet.isUserInteractionEnabled = true
                    storeoutlet.isUserInteractionEnabled = true
     }
+    
+    func responsive() {
+                 
+                 let screenheight = view.frame.size.height
+                 let screenwidth = view.frame.size.width
+                 let ratio = screenheight + screenwidth
+                 
+                 if 1042...1150 ~= ratio  { // iPhone 6 - 6 Plus - 6S - 6S Plus - 7 - 7 Plus - 8 - 8 Plus Series
+                     print("iPhone 6 - 6 Plus - 6S - 6S Plus - 7 - 7 Plus - 8 - 8 Plus Series")
+                 }
+                 else if ratio == 888 { // iPhone 5 - 5S - 5C - SE Series
+                     print("iPhone 5 - 5S - 5C - SE Series")
+                 }
+                 else if ratio == 1187 || ratio == 1310 { // iPhone X - XS - 11 Pro Series +
+                     print("iPhone X - XS - 11 Pro Series")
+                    
+                    shoptext.font = shoptext.font.withSize(70)
+                    
+                    homeoutlet.frame = CGRect(x: homeoutlet.frame.origin.x, y: homeoutlet.frame.origin.y, width: homeoutlet.frame.width, height: homeoutlet.frame.width)
+                    hometext.frame = CGRect(x: hometext.frame.origin.x, y: homeoutlet.frame.maxY, width: hometext.frame.width, height: hometext.frame.height)
+                    
+                    freeoutlet.frame = CGRect(x: freeoutlet.frame.origin.x, y: freeoutlet.frame.origin.y, width: freeoutlet.frame.width, height: freeoutlet.frame.width)
+                    freetext.frame = CGRect(x: freetext.frame.origin.x, y: freeoutlet.frame.maxY, width: freetext.frame.width, height: freetext.frame.height)
+                    
+                    coinoutlet.frame = CGRect(x: coinoutlet.frame.origin.x, y: coinoutlet.frame.origin.y, width: coinoutlet.frame.width, height: coinoutlet.frame.width)
+                    coinstext.frame = CGRect(x: coinstext.frame.origin.x, y: coinoutlet.frame.maxY, width: coinstext.frame.width, height: coinstext.frame.height)
+                    
+                    storeoutlet.frame = CGRect(x: storeoutlet.frame.origin.x, y: storeoutlet.frame.origin.y, width: storeoutlet.frame.width, height: storeoutlet.frame.width)
+                    storetext.frame = CGRect(x: storetext.frame.origin.x, y: storeoutlet.frame.maxY, width: storetext.frame.width, height: storetext.frame.height)
+                    
+                    let buyoneheight = buyoneoutlet.frame.width / 5.1785
+                    buyoneoutlet.frame = CGRect(x: buyoneoutlet.frame.origin.x, y: buyoneoutlet.frame.origin.y, width: buyoneoutlet.frame.width, height: buyoneheight)
+                    
+                    freechance.frame = CGRect(x: freechance.frame.origin.x, y: freechance.frame.origin.y - 30, width: freechance.frame.width, height: freechance.frame.height)
+                    
+                    let buytwoheight = buytwooutlet.frame.width / 2.875
+                    buytwooutlet.frame = CGRect(x: buytwooutlet.frame.origin.x, y: freechance.frame.maxY - 55, width: buytwooutlet.frame.width, height: buytwoheight)
+                    
+                    freeagainturn.frame = CGRect(x: freeagainturn.frame.origin.x, y: freeagainturn.frame.origin.y - 30, width: freeagainturn.frame.width, height: freeagainturn.frame.height)
+                    
+                    let buythreeheight = buythreeoutlet.frame.width / 2.875
+                    buythreeoutlet.frame = CGRect(x: buythreeoutlet.frame.origin.x, y: freeagainturn.frame.maxY - 55, width: buythreeoutlet.frame.width, height: buythreeheight)
+                    
+                    freehint.frame = CGRect(x: freehint.frame.origin.x, y: freehint.frame.origin.y - 30, width: freehint.frame.width, height: freehint.frame.height)
+                    
+                    let buyfourheight = buyfouroutlet.frame.width / 2.875
+                    buyfouroutlet.frame = CGRect(x: buyfouroutlet.frame.origin.x, y: freehint.frame.maxY - 55, width: buyfouroutlet.frame.width, height: buyfourheight)
+                    
+                    warningcloseoutlet.frame = CGRect(x: warningcloseoutlet.frame.origin.x, y: warningcloseoutlet.frame.origin.y, width: warningcloseoutlet.frame.width, height: warningcloseoutlet.frame.width)
+                 }/*
+                 else if ratio == 1310 { // iPhone XR - XS Max - 11 - 11 Pro Max
+                  print("iPhone XR - XS Max - 11 - 11 Pro Max")
+                 }
+                 else if ratio == 2028 { // iPad Pro 11 inch
+                     print("iPad Pro 11 inch")
+                 }
+                 else if ratio == 2390 { // iPad Pro 12.9 inch
+                     print("iPad Pro 12.9 inch")
+                 } */
+                 else if 1792...2390 ~= ratio { // iPad Series +
+                    
+                    shoptext.font = shoptext.font.withSize(view.frame.width / 7)
+                    
+                    let coinwidth = coinoutlet.frame.height
+                    coinoutlet.frame = CGRect(x: view.frame.width / 2 - coinwidth / 2 - 65, y: coinoutlet.frame.origin.y, width: coinwidth, height: coinwidth)
+                    
+                    let freewidth = freeoutlet.frame.height
+                    freeoutlet.frame = CGRect(x: view.frame.width / 2 - freewidth / 2 + 65, y: freeoutlet.frame.origin.y, width: freewidth, height: freewidth)
+                    
+                    let homewidth = homeoutlet.frame.height
+                    homeoutlet.frame = CGRect(x: view.frame.width / 2 - homewidth / 2 - 195, y: homeoutlet.frame.origin.y, width: homewidth, height: homewidth)
+                    
+                    let storewidth = storeoutlet.frame.height
+                    storeoutlet.frame = CGRect(x: view.frame.width / 2 - storewidth / 2 + 195, y: storeoutlet.frame.origin.y, width: storewidth, height: storewidth)
+                    
+                    coinstext.font = coinstext.font.withSize(20)
+                    freetext.font = coinstext.font.withSize(20)
+                    hometext.font = coinstext.font.withSize(20)
+                    storetext.font = coinstext.font.withSize(20)
+                    
+                    coinstext.frame = CGRect(x: coinoutlet.frame.midX - coinstext.frame.width / 2, y: coinstext.frame.origin.y, width: coinstext.frame.width, height: coinstext.frame.height)
+                    
+                     hometext.frame = CGRect(x: homeoutlet.frame.midX - hometext.frame.width / 2, y: hometext.frame.origin.y, width: hometext.frame.width, height: hometext.frame.height)
+                    
+                    freetext.frame = CGRect(x: freeoutlet.frame.midX - freetext.frame.width / 2, y: freetext.frame.origin.y, width: freetext.frame.width, height: freetext.frame.height)
+                    
+                     storetext.frame = CGRect(x: storeoutlet.frame.midX - storetext.frame.width / 2, y: storetext.frame.origin.y, width: storetext.frame.width, height: storetext.frame.height)
+                    
+                    let goldboilerwidth = goldboiler.frame.width * 0.95
+                    let goldboilerheight = goldboilerwidth / 2.5317
+                    goldboiler.frame = CGRect(x: view.frame.width / 2 - goldboilerwidth / 2, y: goldboiler.frame.origin.y, width: goldboilerwidth, height: goldboilerheight)
+                    
+                    let buyonehight = buyoneoutlet.frame.width / 5.1785
+                    buyoneoutlet.frame = CGRect(x: buyoneoutlet.frame.origin.x, y: goldboiler.frame.midY + 30, width: buyoneoutlet.frame.width, height: buyonehight)
+                    
+                    let freeagainturnwidth = freeagainturn.frame.width * 1.8
+                    let freeagainturnheight = freeagainturnwidth / 1.4803
+                    freeagainturn.frame = CGRect(x: view.frame.width / 2 - freeagainturnwidth / 2, y: goldboiler.frame.maxY + 40, width: freeagainturnwidth, height: freeagainturnheight)
+                    
+                    let buythreewidth = buythreeoutlet.frame.width * 0.8
+                    let buythreeheight = buythreewidth / 2.807
+                    buythreeoutlet.frame = CGRect(x: freeagainturn.frame.midX - buythreewidth / 2, y: freeagainturn.frame.maxY - 65, width: buythreewidth, height: buythreeheight)
+                    
+                    let freechancewidth = freechance.frame.width * 1.8
+                    let freechanceheight = freechancewidth / 1.4803
+                    freechance.frame = CGRect(x: view.frame.width / 2 - freechancewidth / 2 - 200, y: goldboiler.frame.maxY + 40, width: freechancewidth, height: freechanceheight)
+                    
+                    let buytwowidth = buytwooutlet.frame.width * 0.8
+                    let buytwoheight = buytwowidth / 2.807
+                    buytwooutlet.frame = CGRect(x: freechance.frame.midX - buytwowidth / 2, y: freechance.frame.maxY - 65, width: buytwowidth, height: buytwoheight)
+                    
+                    let freehintwidth = freehint.frame.width * 1.8
+                    let freehintheight = freehintwidth / 1.4803
+                    freehint.frame = CGRect(x: view.frame.width / 2 - freehintwidth / 2 + 200, y: goldboiler.frame.maxY + 40, width: freehintwidth, height: freehintheight)
+                    
+                    let buyfourwidth = buyfouroutlet.frame.width * 0.8
+                    let buyfourheight = buyfourwidth / 2.807
+                    buyfouroutlet.frame = CGRect(x: freehint.frame.midX - buyfourwidth / 2, y: freehint.frame.maxY - 65, width: buyfourwidth, height: buyfourheight)
+                    
+                    warningcloseoutlet.frame = CGRect(x: warningcloseoutlet.frame.origin.x, y: warningcloseoutlet.frame.origin.y, width: warningcloseoutlet.frame.width, height: warningcloseoutlet.frame.width)
+                    
+                    if ratio == 2390 { // iPad Pro 12.9 inch
+                        print("iPad Pro 12.9 inch")
+                        
+                        let buythreewidth = buythreeoutlet.frame.width * 0.9
+                        let buythreeheight = buythreewidth / 2.807
+                        buythreeoutlet.frame = CGRect(x: freeagainturn.frame.midX - buythreewidth / 2, y: freeagainturn.frame.maxY - 80, width: buythreewidth, height: buythreeheight)
+                        
+                        freechance.frame = CGRect(x: view.frame.width / 2 - freechance.frame.width / 2 - 250, y: goldboiler.frame.maxY + 40, width: freechance.frame.width, height: freechance.frame.height)
+                        
+                        let buytwowidth = buytwooutlet.frame.width * 0.9
+                        let buytwoheight = buytwowidth / 2.807
+                        buytwooutlet.frame = CGRect(x: freechance.frame.midX - buytwowidth / 2, y: freechance.frame.maxY - 80, width: buytwowidth, height: buytwoheight)
+                        
+                        freehint.frame = CGRect(x: view.frame.width / 2 - freehint.frame.width / 2 + 250, y: goldboiler.frame.maxY + 40, width: freehint.frame.width, height: freehint.frame.height)
+                        
+                        let buyfourwidth = buyfouroutlet.frame.width * 0.9
+                        let buyfourheight = buyfourwidth / 2.807
+                        buyfouroutlet.frame = CGRect(x: freehint.frame.midX - buyfourwidth / 2, y: freehint.frame.maxY - 80, width: buyfourwidth, height: buyfourheight)
+                    }
+                 }
+             }
 }
